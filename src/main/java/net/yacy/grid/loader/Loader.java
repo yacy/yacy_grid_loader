1c01aa55c97a228659be3ccb496adf927e62af98
Michael Peter Christen <mc@yacy.net>
2017-05-24 13:37:08 +0200

    fix for new server html option

--- src/main/java/net/yacy/grid/loader/Loader.java
+++ src/main/java/net/yacy/grid/loader/Loader.java
@@ -143,7 +143,7 @@ public class Loader {
         List<Class<? extends Servlet>> services = new ArrayList<>();
         services.addAll(Arrays.asList(MCP.MCP_SERVICES));
         services.addAll(Arrays.asList(LOADER_SERVICES));
-        Service.runService(SERVICE, DATA_PATH, APP_PATH, services);
+        Service.runService(SERVICE, DATA_PATH, APP_PATH, null, services);
         brokerListener.terminate();
     }
     

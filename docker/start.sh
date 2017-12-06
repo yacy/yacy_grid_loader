8685a7e2a6b896172d23623d937bab30bf14c424
Christian Meier <meier@publicplan.de>
2017-12-06 15:07:44 +0100

    add log folder add net-tools

--- docker/start.sh
+++ docker/start.sh
@@ -3,5 +3,5 @@ adduser --disabled-password --gecos '' r
 adduser r sudo
 echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
 cd /yacy_grid_loader
-sleep 5s;
-gradle run
+sleep 1s;
+gradle run > ./log/yacy-crawler.log

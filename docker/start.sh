ee0bd92cec47bef69a0cf3db84bce8ea2ef62dfc
Maximilian Schorradt <schorradt@publicplan.de>
2018-09-12 16:00:41 +0200

    Stelle auf openjdk-8 & gradle Docker Image um

--- docker/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-adduser --disabled-password --gecos '' r
-adduser r sudo
-echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
-cd /yacy_grid_loader
-sleep 1s;
-gradle run > ./log/yacy-loader.log
